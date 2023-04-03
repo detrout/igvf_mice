4295335c94ff4097452149d01ab14a683268da0e
Diane Trout <diane@ghic.org>
2023-04-03 15:10:10 -0700

    Remember how to do django model tests

--- igvf_mice/tests.py
+++ igvf_mice/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import (
+    AccessionNamespace,
+    Accession,
+    Source,
+
+)
+
+
+class TestAccession(TestCase):
+    def setUp(self):
+        self.igvf = AccessionNamespace.objects.create(name="IGVF", accession_prefix="igvf")
+        self.igvf_test = AccessionNamespace.objects.create(name="IGVF test", accession_prefix="igvf_test")
+
+    def test_accession(self):
+        test016_B6J_10F = Accession.objects.create(
+            namespace=self.igvf_test,
+            name="TSTDO36427294",
+            url="https://api.sandbox.igvf.org/rodent-donors/TSTDO36427294/")
+
+        self.assertEqual(str(test016_B6J_10F), "igvf_test:TSTDO36427294")
+        self.assertEqual(test016_B6J_10F.path, "/rodent-donors/TSTDO36427294/")
