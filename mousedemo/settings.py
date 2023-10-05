93f142b97ebc72dede1038707450dc8d5717f841
Diane Trout <diane@ghic.org>
2023-10-05 11:38:49 -0700

    Add default render classes for django rest framework

--- mousedemo/settings.py
+++ mousedemo/settings.py
@@ -132,6 +132,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
     "URL_FIELD_NAME": "@id",
 }
 
