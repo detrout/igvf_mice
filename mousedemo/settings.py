005498bc47b71b79d7b4bd14cd21ba3e74a4ef4d
Diane Trout <diane@ghic.org>
2023-10-05 16:44:46 -0700

    Enable API pagination

--- mousedemo/settings.py
+++ mousedemo/settings.py
@@ -136,6 +136,8 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    "PAGE_SIZE": 100,
     "URL_FIELD_NAME": "@id",
 }
 
