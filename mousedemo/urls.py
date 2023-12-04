27ffd5c7605ca5552b5a602f184235cc95bbf010
Diane Trout <diane@ghic.org>
2023-12-04 11:16:47 -0800

    Include the main router endpoints

--- mousedemo/urls.py
+++ mousedemo/urls.py
@@ -54,4 +54,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("", include(router.urls)),
 ]
