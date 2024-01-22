a0092518c388af92748601e271a438a909ca4f71
Diane Trout <diane@ghic.org>
2024-01-22 11:19:06 -0800

    Use double quotes to be more consistent with rest of file

--- mousedemo/settings.py
+++ mousedemo/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
     "URL_FIELD_NAME": "@id",
 }
