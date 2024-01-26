547a6a146321884c7812fd0d52ad03c138494c45
Diane Trout <diane@ghic.org>
2024-01-26 10:42:26 -0800

    Parse and test the plate layout sheet
    Hopefully more tests for the sub components and more auto-detection of
    plate layout variations will make the parser more reliable

--- igvf_mice/io/read_sheet.py
+++ igvf_mice/io/read_sheet.py
@@ -1,7 +1,13 @@
+from collections import namedtuple
+import functools
 import pandas
+import re
 
 from .. import models
 from .converters import (
+    normalize_plate_name,
+    parse_mouse_tissue,
+    get_genotype_from_mouse_tissue,
     str_or_empty,
     uci_tz_or_none,
 )
@@ -174,3 +180,235 @@ def import_tissues(tissue_sheets, submitted_tissues=None):
         import_accessions(submitted_accessions.get(row["Mouse_Tissue ID"]), record)
 
     assert not failed, "Check warning messages."
+
+WellContent = namedtuple("well_content", ["genotype", "tissue_id"])
+
+class PlateLayoutParser:
+    def __init__(self):
+        self.plate_label = 1
+        self.column_label = 1
+        self.well_row_label_column = 1
+        self.well_row_id_column = 2
+        self.well_start_column = 3
+        self.well_end_column = self.well_start_column + 11
+        self.well_column_range = slice(self.well_start_column, self.well_end_column)
+
+        # for the pooled wells
+        self._pooled_well_re = re.compile("^([A-H]+)_(?P<sex>[MF])[0-9]_([0-9])+$")
+        self._well_id_re = re.compile("^[A-H]1?[\d]$")
+
+        # Used for validation rules
+        self._mouse_strains = {x.name for x in models.MouseStrain.objects.all()}
+        self._sex_re = re.compile("^(Tissue[0-9]_)?(?P<sex>[MF])(_rep[0-9]+)?$")
+
+    def find_plate_start(self, sheet):
+        """Search for the start of a plate layout block
+        """
+        for plate_id_row in sheet[sheet[self.plate_label].apply(self.is_plate_name)].index:
+            plate_name = normalize_plate_name(sheet.iloc[plate_id_row][self.plate_label])
+            if plate_name.endswith("XX"):
+                continue
+
+            column_header = [str(x) for x in range(1,13)]
+            for i in range(plate_id_row, plate_id_row + 4):
+                current_header = [str(x) for x in sheet.loc[i, self.well_column_range]]
+                if current_header == column_header:
+                    yield (plate_name, i-1)
+
+    def get_block_column_ids(self, sheet, block_row_start):
+        label_start_row = block_row_start + 1
+
+        for cell in sheet.iloc[label_start_row, self.well_start_column:]:
+            if pandas.notnull(cell):
+                yield cell
+            else:
+                break
+
+    def get_block_column_labels(self, sheet, block_row_start):
+        id_start_row = block_row_start
+
+        for cell in sheet.iloc[id_start_row, self.well_start_column:]:
+            if pandas.notnull(cell):
+                yield cell
+            else:
+                break
+
+    def get_block_simple_column_end(self, sheet, block_row_start):
+        """Some plates have mixed genotype wells which are hard to validate
+        """
+        data_start_row = block_row_start + 2
+
+        for col_offset in range(self.well_start_column, 255):
+            cell = sheet.iloc[data_start_row, col_offset]
+            if pandas.isnull(cell) or self._pooled_well_re.match(cell):
+                return col_offset
+
+    def get_block_row_ids(self, sheet, block_row_start):
+        label_start_row = block_row_start + 2
+
+        started = False
+        for i, row in sheet.iloc[label_start_row:].iterrows():
+            if pandas.notnull(row[self.well_row_id_column]) and pandas.notnull(row[self.well_start_column]):
+                started = True
+                yield row[self.well_row_id_column]
+            # if we've seen the start of the row ids, and we hit a blank we should stop
+            elif started:
+                break
+
+    def get_block_row_labels(self, sheet, block_row_start):
+        label_start_row = block_row_start + 2
+
+        started = False
+        for i, row in sheet.iloc[label_start_row:].iterrows():
+            if pandas.notnull(row[self.well_row_label_column]) and pandas.notnull(row[self.well_start_column]):
+                started = True
+                yield row[self.well_row_label_column]
+            # if we've seen the start of the row ids, and we hit a blank we should stop
+            elif started:
+                break
+
+    def get_merged_well_contents(self, sheet, block_row_start):
+        """Return the well content lists for well containing multiple tissues"""
+        well_end_column = self.get_block_simple_column_end(sheet, block_row_start)
+        well_range = range(self.well_start_column, well_end_column)
+
+        column_labels = list(self.get_block_column_labels(sheet, block_row_start))
+        column_validators = list(self.get_validation_label_rules(column_labels))
+
+        row_labels = list(self.get_block_column_labels(sheet, block_row_start))
+        row_validators = list(self.get_validation_label_rules(row_labels))
+
+        well_contents = {}
+        row_offset = 0
+        for start in self.get_merged_well_definition_start(sheet, block_row_start):
+            # the well ids like A9..B12 are 3 lines down from the start
+            well_ids = []
+            for cell in sheet.iloc[start+3][well_range]:
+                if pandas.notnull(cell) and self._well_id_re.match(cell):
+                    row = cell[0]
+                    column = cell[1:]
+                    well_ids.append((row, column))
+                else:
+                    print(f"Well id {cell} failed validation on 0-based line {start+3}")
+                    return
+
+            for pooled_row in [start, start+1]:
+                current_sheet_slice = sheet.iloc[pooled_row][well_range]
+                for col_offset, (well_id, cell) in enumerate(zip(well_ids, current_sheet_slice)):
+                    # Really could use some validation rules here...
+                    if callable(row_validators[row_offset]):
+                        if not row_validators[row_offset](cell):
+                            print(f"Failed row_validator[{row_offset},{col_offset}]({cell})")
+                    if callable(column_validators[col_offset]):
+                        if not column_validators[col_offset](cell):
+                            print(f"Failed column_validator[{row_offset},{col_offset}]({cell})")
+                    genotype = get_genotype_from_mouse_tissue(cell)
+
+                    well_contents.setdefault(well_id, []).append(WellContent(genotype, cell))
+                row_offset += 1
+
+        return well_contents
+
+    def get_merged_well_definition_start(self, sheet, block_row_start):
+        """Return the sheet locations where we can find the contents of merged wells.
+        """
+        detail_start_row = block_row_start + len(list(self.get_block_row_ids(sheet, block_row_start)))
+        maximum_search_length = 30
+        detail_max_row = min(detail_start_row + maximum_search_length, sheet.shape[0])
+        detail_range = range(detail_start_row, detail_max_row)
+
+        merged_well_ids = set(self.get_merged_well_ids(sheet, block_row_start))
+        if len(merged_well_ids) == 0:
+            return
+
+        definition_start = detail_start_row
+        for row_index in detail_range:
+            cell = sheet.iloc[row_index][self.well_start_column]
+            if pandas.notnull(cell) and cell in merged_well_ids:
+                yield definition_start - 2
+            definition_start += 1
+
+    def get_merged_well_ids(self, sheet, block_row_start):
+        """Extract the ids used to refer to the tissue ids that were pooled
+        """
+        pooled_start_row = block_row_start + 2
+        pooled_start_column = self.get_block_simple_column_end(sheet, block_row_start)
+
+        for row_index, row in sheet.iloc[pooled_start_row:,pooled_start_column:].iterrows():
+            saw_data = False
+            for col_offset, cell in enumerate(row):
+                if pandas.notnull(cell):
+                    if self._pooled_well_re.match(cell):
+                        saw_data = True
+                        yield cell
+                else:
+                    continue
+            if not saw_data:
+                break
+
+    @staticmethod
+    def _validate_sex(value, expected_sex):
+        try:
+            tissue = parse_mouse_tissue(value)
+            sex = tissue.mouse_age_sex[-1]
+        except ValueError:
+            return False
+        return sex == expected_sex
+
+    @staticmethod
+    def _validate_strain(value, expected_strain):
+        try:
+            tissue = parse_mouse_tissue(value)
+            strain = tissue.mouse_strain
+        except ValueError:
+            return False
+
+        return strain == expected_strain
+
+    def get_validation_label_rules(self, labels):
+        for l in labels:
+            sex = self._sex_re.match(l)
+            if sex:
+                yield functools.partial(PlateLayoutParser._validate_sex, expected_sex=sex.group("sex"))
+            elif l in self._mouse_strains:
+                yield functools.partial(PlateLayoutParser._validate_strain, expected_strain=l)
+            else:
+                yield None
+
+    def get_well_contents_from_block(self, sheet, plate_start):
+        column_ids = list(self.get_block_column_ids(sheet, plate_start))
+        column_labels = list(self.get_block_column_labels(sheet, plate_start))
+        column_validators = list(self.get_validation_label_rules(column_labels))
+        column_end = self.get_block_simple_column_end(sheet, plate_start)
+
+        row_ids = list(self.get_block_row_ids(sheet, plate_start))
+        row_labels = list(self.get_block_row_labels(sheet, plate_start))
+        row_validators = list(self.get_validation_label_rules(row_labels))
+
+        data_row_start = plate_start + 2
+        data_row_range = slice(data_row_start, data_row_start + len(row_labels))
+        column_range = slice(self.well_start_column, column_end)
+
+        well_contents = {}
+        for row_offset, (i, row) in enumerate(sheet.iloc[data_row_range, column_range].iterrows()):
+            for col_offset, cell in enumerate(row):
+                #print(cell)
+                if callable(row_validators[row_offset]):
+                    if not row_validators[row_offset](cell):
+                        print(f"Failed row_validator[{row_offset},{col_offset}]({cell})")
+                if callable(column_validators[col_offset]):
+                    if not column_validators[col_offset](cell):
+                        print(f"Failed column_validator[{row_offset},{col_offset}]({cell})")
+                genotype = get_genotype_from_mouse_tissue(cell)
+
+                row_id = str(row_ids[row_offset])
+                col_id = str(column_ids[col_offset])
+                well_contents[row_id, col_id] = [WellContent(genotype, cell)]
+
+
+        #well_contents.update(self.get_merged_well_contents(sheet, plate_start))
+
+        return well_contents
+
+    def is_plate_name(self, name):
+        return not pandas.isnull(name) and name.startswith("IGVF_")
